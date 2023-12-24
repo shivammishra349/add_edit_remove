e05c8fb0a8e717cf144700a83113f00539c4c411
shivammishra349 <shivam9.mishra1999@gmail.com>
2023-12-24 12:27:58 +0530

    done

---
+++ index.js
@@ -0,0 +1,49 @@
+function project(event){
+    event.preventDefault()
+    let number=document.getElementById('num').value
+   let item=document.getElementById('expance-name').value;
+   let list=document.getElementById('expence').value
+   let obj={
+    number:number,
+    list:list,
+    name:item
+    
+   }
+   localStorage.setItem(number,JSON.stringify(obj));
+   showuser(obj)
+}
+function showuser(obj){
+    let parent=document.getElementById('listOfItem');
+    let li=document.createElement('li');
+    li.textContent=obj.number+" - "+ obj.list+ " - "+obj.name;
+    //parent.appendChild(li);
+
+    let deletebtn=document.createElement('input');
+    deletebtn.type="button";
+    deletebtn.value="delete";
+    li.appendChild(deletebtn);
+    parent.appendChild(li)
+
+    deletebtn.onclick=()=>{
+        localStorage.removeItem(obj.number);
+        parent.removeChild(li)
+    }
+
+
+    let editbtn=document.createElement('input');
+    editbtn.type="button";
+    editbtn.value="edit"
+    li.appendChild(editbtn);
+    parent.appendChild(li)
+
+
+    editbtn.onclick=()=>{
+        localStorage.removeItem(obj.number);
+        parent.removeChild(li);
+
+        document.getElementById('num').value=obj.number;
+        document.getElementById('expance-name').value=obj.name;
+        document.getElementById('expence').value=obj.list
+    }
+
+}
